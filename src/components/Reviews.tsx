149d1cb5976e7510658d044f685f87c7e3fd00af
RabGH <rabii.ghais@gmail.com>
2024-08-16 11:23:50 +0300

    Reviews section, framer motion install, icons file

---
+++ src/components/Reviews.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { HTMLAttributes, useEffect, useRef, useState } from "react";
+import { useInView } from "framer-motion";
+
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { cn } from "@/lib/utils";
+
+const PHONES = [
+  "/testimonials/1.jpg",
+  "/testimonials/2.jpg",
+  "/testimonials/3.jpg",
+  "/testimonials/4.jpg",
+  "/testimonials/5.jpg",
+  "/testimonials/6.jpg",
+];
+
+function splitArray<T>(array: Array<T>, numParts: number) {
+  // Purpose: This function splits an array into a specified number of subarrays (numParts).
+  // Logic: It iterates through the array, distributing each item into one of the numParts subarrays.
+  const result: Array<Array<T>> = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const index = i % numParts;
+    if (!result[index]) {
+      result[index] = [];
+    }
+    result[index].push(array[i]);
+  }
+  return result;
+}
+
+function ReviewColumn({
+  reviews,
+  className,
+  reviewClassName,
+  msPerPixel = 0,
+}: {
+  reviews: string[];
+  className?: string;
+  reviewClassName?: (reviewIndex: number) => string;
+  msPerPixel?: number;
+}) {
+  // Ref for the div we are watching which is the column of the images
+  const columnRef = useRef<HTMLDivElement | null>(null);
+  // Check for the size of the column to set the duration / speed of the animation on different devices
+  const [columnHeight, setColumnHeight] = useState(0);
+  // observes when the div element using the columnRef is resized and when it is we offset it's height
+  // Now we know the height of the column, we can set the duration / speed by multiplying per pixel of the column height
+  const duration = `${columnHeight * msPerPixel}ms`;
+
+  useEffect(() => {
+    if (!columnRef.current) return;
+
+    const resizeObserver = new window.ResizeObserver(() => {
+      setColumnHeight(columnRef.current?.offsetHeight ?? 0);
+    });
+
+    resizeObserver.observe(columnRef.current);
+    // stay clean, disconnect the useEffect after it runs
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, []);
+
+  return (
+    <div
+      ref={columnRef}
+      className={cn("animate-marquee space-y-8 py-4", className)}
+      style={{ "--marquee-duration": duration } as React.CSSProperties}
+    ></div>
+  );
+}
+
+function ReviewGrid() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const isInView = useInView(containerRef, { once: true, amount: 0.4 });
+  const columns = splitArray(PHONES, 3);
+  const columns1 = columns[0];
+  const columns2 = columns[1];
+  const columns3 = splitArray(columns[2], 2);
+
+  return (
+    <div
+      ref={containerRef}
+      className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+    >
+      {isInView ? (
+        <>
+          <ReviewColumn />
+        </>
+      ) : null}
+    </div>
+  );
+}
+
+export function Reviews() {
+  return (
+    <MaxWidthWrapper>
+      <img
+        aria-hidden="true"
+        src="/what-people-are-buying.png"
+        className="absolute select-none hidden xl:block -left-32 top-1/3"
+      />
+
+      <ReviewGrid />
+    </MaxWidthWrapper>
+  );
+}
