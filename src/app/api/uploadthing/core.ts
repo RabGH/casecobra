0a421947bdd83d676f4a6a088f33e0098aeb377b
RabGH <rabii.ghais@gmail.com>
2024-08-20 21:23:14 +0300

    Upload loading progress, uploadthing image upload frontend logic, toast notification, error handling

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,33 +1,20 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { z } from "zod";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
-
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-    // Set permissions and file types for this FileRoute
-    .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth(req);
-
-      // If you throw, the user will not be able to upload
-      if (!user) throw new UploadThingError("Unauthorized");
-
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
+    // take & validate input
+    .input(z.object({ configId: z.string().optional() }))
+    // input it into middleware
+    .middleware(async ({ input }) => {
+      return { input };
     })
+    // middle ware to handler, after image is completely uploaded
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      //   console.log("Upload complete for userId:", metadata.userId);
-
-      //   console.log("file url", file.url);
-
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
+      const { configId } = metadata.input;
+      return { configId };
     }),
 } satisfies FileRouter;
 
