c154fe81509a668e530587f3c3c607ef7f028c74
RabGH <rabii.ghais@gmail.com>
2024-08-25 18:57:27 +0300

    Aspect ratio design page elements, configure cropped image

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,5 +1,43 @@
-const DesignPage = async () => {
-  return <div></div>;
+import { notFound } from "next/navigation";
+
+import { db } from "@/db";
+import DesignConfigurator from "./DesignConfigurator";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const DesignPage = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imgUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={""}
+      imgUrl={""}
+      imageDimensions={{
+        width: 0,
+        height: 0,
+      }}
+    />
+  );
 };
 
 export default DesignPage;
