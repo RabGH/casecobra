c68983f7102fc213edfbbfe133f464fbce674b0a
RabGH <rabii.ghais@gmail.com>
2024-12-13 00:08:27 +0200

    Login Modal dialog / call back auth function

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,7 +1,15 @@
 import type { Dispatch, SetStateAction } from "react";
+import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs";
 import Image from "next/image";
 
-import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { buttonVariants } from "./ui/button";
 
 const LoginModal = ({
   isOpen,
@@ -22,7 +30,24 @@ const LoginModal = ({
               fill
             />
           </div>
+          <DialogTitle className="text-center text-3xl font-bold tracking-tight text-gray-900">
+            Log in to continue
+          </DialogTitle>
+          <DialogDescription className="py-2 text-center text-base">
+            <span className="font-medium text-zinc-900">
+              Your configuration was saved!
+            </span>{" "}
+            Please login or create an account to complete your purchase.
+          </DialogDescription>
         </DialogHeader>
+        <div className="grid grid-cols-2 gap-6 divide-x divide-gray-200">
+          <LoginLink className={buttonVariants({ variant: "outline" })}>
+            Login
+          </LoginLink>
+          <RegisterLink className={buttonVariants({ variant: "default" })}>
+            Sign up
+          </RegisterLink>
+        </div>
       </DialogContent>
     </Dialog>
   );
